3eef60c974f1ce1e70e7fb191384c82e922f3b18
Ihor Honchar <ihor693@gmail.com>
2020-11-04 23:56:29 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/controllers/BlogController.java
@@ -0,0 +1,87 @@
+package com.example.demo.controllers;
+
+import com.example.demo.models.Post;
+import com.example.demo.repo.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+@Controller
+public class BlogController {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @GetMapping("/")
+    public String getBlog(Model model){
+        Iterable<Post> posts = postRepository.findAll();
+        model.addAttribute("posts", posts);
+        return "blog";
+    }
+
+    @GetMapping("/blog/add")
+    public String blogAdd(Model model){
+        return "blog-add";
+    }
+    @PostMapping("/blog/add")
+    public String blogPostAdd( @RequestParam String title, @RequestParam String anons, @RequestParam String full_text, Model model){
+        Post post = new Post(title, anons, full_text);
+        postRepository.save(post);
+        return "redirect:/blog";
+    }
+
+    @GetMapping("/blog/{id}")
+    public String blogDetails( @PathVariable(value = "id") long id, Model model) throws Exception {
+
+        if (!postRepository.existsById(id)) {
+            return "redirect:/blog";
+        }
+
+        Post post = postRepository.findById(id).orElseThrow(() -> new Exception("Post not found"));
+        int count = post.getViews() + 1;
+        post.setViews(count);
+        postRepository.save(post);
+
+        Optional<Post> showPost = postRepository.findById(id);
+        ArrayList<Post> res = new ArrayList<>();
+        showPost.ifPresent(res::add);
+        model.addAttribute("post", res);
+        return "blog-details";
+    }
+    @GetMapping("/blog/{id}/edit")
+    public String blogEdit( @PathVariable(value = "id") long id, Model model){
+
+        if (!postRepository.existsById(id)) {
+            return "redirect:/blog";
+        }
+
+        Optional<Post> post = postRepository.findById(id);
+        ArrayList<Post> res = new ArrayList<>();
+        post.ifPresent(res::add);
+        model.addAttribute("post", res);
+        return "blog-edit";
+    }
+    @PostMapping("/blog/{id}/edit")
+    public String blogPostUpdate(@PathVariable(value = "id") long id, @RequestParam String title, @RequestParam String anons, @RequestParam String full_text, Model model) throws Exception {
+        Post post = postRepository.findById(id).orElseThrow(() -> new Exception("Post not found"));
+        post.setTitle(title);
+        post.setAnons(anons);
+        post.setFull_text(full_text);
+        postRepository.save(post);
+        return "redirect:/blog";
+    }
+
+    @PostMapping("/blog/{id}/remove")
+    public String blogPostDelete(@PathVariable(value = "id") long id, Model model) throws Exception {
+        Post post = postRepository.findById(id).orElseThrow(() -> new Exception("Post not found"));
+        postRepository.delete(post);
+        return "redirect:/blog";
+    }
+}
